044a5ffa9fddce3f874d901f0c3673477bf77766
fahmi <fahmi@radio.cloud>
2025-02-06 10:21:02 +0700

    sCurveFadeIn 20%

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ var FADEIN = exports.FADEIN = "FadeIn";
 var FADEOUT = exports.FADEOUT = "FadeOut";
 
 function sCurveFadeIn(start, duration) {
-    var curve = (0, _fadeCurves.sCurve)(10000, 1);
+    var curve = (0, _fadeCurves.sCurve)(10000, 0.2); // Ends at 0.2
 
-    console.log(curve, 'curve')
+    console.log('20%!!!!')
     this.setValueCurveAtTime(curve, start, duration);
 }
 
